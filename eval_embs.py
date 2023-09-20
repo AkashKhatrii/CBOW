d421da54102f396f2dc62a0e1fd45861e079cab4
AkashKhatrii <65164913+AkashKhatrii@users.noreply.github.com>
2023-09-20 14:02:26 -0600

    Update paths to test files

--- eval_embs.py
+++ eval_embs.py
@@ -6,8 +6,8 @@ import os
 
 def get_eval_stats(emb_file):   
     wv = KeyedVectors.load_word2vec_format(datapath(emb_file), binary=False)
-    sim = wv.evaluate_word_pairs(os.getcwd()+"/../test_files/wordsim_similarity_goldstandard.txt")
-    analogy = wv.evaluate_word_analogies(os.getcwd()+"/../test_files/questions-words_headered.txt")
+    sim = wv.evaluate_word_pairs("/scratch/general/vast/u1469481/cs6957/assignment1/models/test_files/wordsim_similarity_goldstandard.txt")
+    analogy = wv.evaluate_word_analogies("/scratch/general/vast/u1469481/cs6957/assignment1/models/test_files/wordsim_similarity_goldstandard.txt")
     print(f"Word Similarity Test Pearson Correlation: {sim[0][0]}")
     print(f"Accuracy on Analogy Test: {analogy[0]}")
 
